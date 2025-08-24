55ebe930b9ee455af485ab278eef02446b18a475
Kaan Küreli <kkureli@gmail.com>
2025-08-25 01:16:36 +0300

    feat(core): error boundary added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import AppNavigator from './src/navigation/RootNavigator';
+import ErrorBoundary from './src/ui/components/ErrorBoundary';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <ErrorBoundary>
+      <AppNavigator />
+    </ErrorBoundary>
+  );
 }
